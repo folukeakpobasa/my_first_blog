b11bfdc15823860b45561a96cc8f1002817b21d3
Decagon <decagon@Decagons-MacBook-Air.local>
2021-09-21 07:05:14 +0100

    My Django Girls app, first commit

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'my_blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
